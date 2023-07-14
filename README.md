e6873c1ba1ee883fea0386304394ac22ff66d050
charla winbush <charla.winbush@outlook.com>
2023-07-13 17:15:43 -0700

    push

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # landing-page
+
+This is the landing Page project readme.
+
+The languages used HTML, CSS, and JAVASCRIPT.
+
+The Functionalities used
+
+The Navigation Bar
+The Scrolling Effect
+The Dynamic active
+
+What did we learn:
+
+In this project we learned how to manipulate the DOM and convert the web project to dynamic project.
--- cd0428-landing-page-main/js/app.js
+++ cd0428-landing-page-main/js/app.js
@@ -1,62 +1,77 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * Define Global Variables
- * 
-*/
-
+ *
+ */
+const navigation = document.getElementById("navbar__list"); //
+const sections = document.querySelectorAll("section");
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
-
-
+ *
+ */
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
-
-
+const navBuilder = () => {
+  sections.forEach((section) => {
+    let navUI = "";
+    const sectionID = section.id;
+    const sectionDataNav = section.dataset.nav;
+    navUI += `<li><a class = "menu__link" href="#${sectionID}">${sectionDataNav}</a></li>;`;
+    navigation.append(navUI);
+  });
+};
+navBuilder();
 // Add class 'active' to section when near top of viewport
-
+const offset = (section) => {
+  return Math.floor(section.getBoundingClientRect().top);
+};
 
 // Scroll to anchor ID using scrollTO event
-
+const scrolling = () => {
+  const links = document.querySelectorAll(".navbar_menu a");
+  links.forEach((link) => {
+    link.addEventListener("click", () => {
+      for (i = 0; i < sections; i++) {}
+    });
+  });
+};
+scrolling();
 
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
