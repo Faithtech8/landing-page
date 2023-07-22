1d431d5a0508124d8ec7eb896d11d2477e73d681
charla winbush <charla.winbush@outlook.com>
2023-07-22 15:48:25 -0700

    push

--- cd0428-landing-page-main/js/app.js
+++ cd0428-landing-page-main/js/app.js
@@ -14,12 +14,11 @@
 /**
  * Global Variables
  */
-const navMenu = document.getElementById('navbar__list');
-const sections = document.querySelectorAll('section');
+const navMenu = document.getElementById("navbar__list");
+const sections = document.querySelectorAll("section");
 /**
  * Helper Functions
  */
-
 
 // Calculate offset from the top of the viewport
 
@@ -29,10 +28,10 @@ const sections = document.querySelectorAll('section');
 
 // Build the navigation menu
 
-const buildNavMenu() {
-  const fragment = doument.createDocumentFragment();
-sections.forEach((section) => {
-    const listItem = document.createElement('li');
+function buildNavMenu() {
+  const fragment = document.createDocumentFragment();
+  sections.forEach((section) => {
+    const listItem = document.createElement("li");
     listItem.innerHTML = `<a class="menu__link" href="#${section.id}">${section.dataset.nav}</a>`;
     fragment.appendChild(listItem);
   });
@@ -41,51 +40,37 @@ sections.forEach((section) => {
 }
 buildNavMenu();
 
+function highlightActiveSection() {
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll("nav a");
 
-    const scrollToAnchor(event) {
-      event.preventDefault();
-      const target = event.target.getAttribute("href");
-      const targetElement = document.querySelector(target);
-      const headerOffset = 50; 
-      const elementPosition = targetElement.getBoundingClientRect().top;
-      const offsetPosition = elementPosition - headerOffset;
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    const scrollPosition = window.scrollY;
 
-      window.scrollBy({
-        top: offsetPosition,
-        behavior: "smooth",
-      });
-    }
-
-    
-    const highlightActiveSection() {
-      const sections = document.querySelectorAll("section");
-      const navLinks = document.querySelectorAll("nav a");
-
-      sections.forEach((section) => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;
-        const scrollPosition = window.scrollY;
-
-        if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
-          navLinks.forEach((link) => {
-            link.classList.remove("active");
-            if (link.getAttribute("href") === `#${section.id}`) {
-              link.classList.add("active");
-            }
-          });
+    if (
+      scrollPosition >= sectionTop &&
+      scrollPosition < sectionTop + sectionHeight
+    ) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === `#${section.id}`) {
+          link.classList.add("active");
         }
       });
     }
+  });
+}
 
-    // Attach click event to navigation links
-    const navLinks = document.querySelectorAll("nav a");
-    navLinks.forEach((link) => {
-      link.addEventListener("click", scrollToAnchor);
-    });
+// Attach click event to navigation links
+const navLinks = document.querySelectorAll("nav a");
+navLinks.forEach((link) => {
+  link.addEventListener("click", scrollToAnchor);
+});
 
-    // Attach scroll event to highlight active section
-    window.addEventListener("scroll", highlightActiveSection);
-  
+// Attach scroll event to highlight active section
+window.addEventListener("scroll", highlightActiveSection);
 
 // Scroll to anchor ID using scrollIntoView event
 const scrollToSection = () => {
@@ -93,22 +78,29 @@ const scrollToSection = () => {
 
   links.forEach((link, index) => {
     link.addEventListener("click", (event) => {
-      event.preventDefault();
+      event.preventDefault(
+      }});
 
       const section = document.getElementById(
         link.getAttribute("href").slice(1)
       );
-      if (section) {
-        section.scrollIntoView({
-          behavior: "smooth",
-        });
-      }
-    });
-  });
-};
+      
+    
+    
+    
+  
+
 
 // Call the function to enable smooth scrolling to anchor IDs
-scrollToSection();
+
+  
+    
+    
+    
+    
+    '
+})
+
 
 /**
  * Events
@@ -133,23 +125,18 @@ const getOffsetFromTop = (element) => {
 };
 
 const setActiveSections = () => {
-    sections.forEach((section) => {
-      setSectionActive(section);
-    });
-  };
-  
-  const setSectionActive = (section) => {
-    const sectionOffset = getOffsetFromTop(section);
-    const isInViewport = sectionOffset < 200 && sectionOffset >= -200; 
-  
-    if (isInViewport) {
-      section.classList.add("your-active-class");
-    } else {
-      section.classList.remove("your-active-class");
-    }
-  };
-  
-  
-  
-  
-  
+  sections.forEach((section) => {
+    setSectionActive(section);
+  });
+};
+
+const setSectionActive = (section) => {
+  const sectionOffset = getOffsetFromTop(section);
+  const isInViewport = sectionOffset < 200 && sectionOffset >= -200;
+
+  if (isInViewport) {
+    section.classList.add("your-active-class");
+  } else {
+    section.classList.remove("your-active-class");
+  }
+};
